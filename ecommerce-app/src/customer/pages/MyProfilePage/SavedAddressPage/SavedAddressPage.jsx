dcd81eaa938444354e6ab7434703a12e256aecdb
ThotaDisylva <thota.disylva@accolitedigital.com>
2024-05-27 16:13:37 +0530

    Update SavedAddressPage.jsx

--- ecommerce-app/src/customer/pages/MyProfilePage/SavedAddressPage/SavedAddressPage.jsx
+++ ecommerce-app/src/customer/pages/MyProfilePage/SavedAddressPage/SavedAddressPage.jsx
@@ -10,6 +10,7 @@ function SavedAddressPage() {
   const [currentAddress, setCurrentAddress] = useState(null);
   const {loading, savedAddresses, addAddress, updateAddress} = useAddressInfo();
   const [addresses, setAddresses] = useState([]);
+  const [showDeletePopup, setShowDeletePopup] = useState(false);
 
   useEffect(() => {
     if (!loading) {
@@ -38,7 +39,18 @@ console.log("savedAddresses",savedAddresses)
     }
     togglePopup();
   };
+  const handleDelete = (id) => {
+    setShowDeletePopup(true);
+  };
 
+  const confirmDelete = () => {
+    setShowDeletePopup(false);
+    deleteAddress();
+  };
+
+  const cancelDelete = () => {
+    setShowDeletePopup(false);
+  };
   return (
     <div className='w-full'>
       {!isOpen && (
@@ -62,7 +74,7 @@ console.log("savedAddresses",savedAddresses)
           </div>
           <div className='w-full lg:w-[90%] h-fit pt-10 pb-5 px-2 md:p-10 space-y-2'>
             {addresses?.map((address) => (
-              <AddressCard key={address.addressId} address={address} onEdit={handleEdit} editable={true}/>
+              <AddressCard key={address.addressId} address={address} onEdit={handleEdit} editable={true} onDelete={handleDelete} />
             ))}
           </div>
         </div>
@@ -70,6 +82,27 @@ console.log("savedAddresses",savedAddresses)
       {isOpen && (
         <div className='flex justify-center'>
           <AddAddressCard address={currentAddress} togglePopup={togglePopup} onSave={handleSave} />
+        </div>
+      )}
+      {showDeletePopup && (
+        <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex items-center justify-center z-50">
+          <div className="bg-white p-6 rounded-lg shadow-lg">
+            <p className="mb-4">Are you sure you want to delete the address?</p>
+            <div className="flex space-x-4">
+              <button
+                onClick={confirmDelete}
+                className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-700"
+              >
+                Yes
+              </button>
+              <button
+                onClick={cancelDelete}
+                className="bg-gray-500 text-white px-4 py-2 rounded-lg hover:bg-gray-700"
+              >
+                No
+              </button>
+            </div>
+          </div>
         </div>
       )}
     </div>
